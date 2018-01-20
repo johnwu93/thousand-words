4a341728064c14fbc093942555fc15c4adc45c1f
Ong Jia Rui <ongjiarui@hotmail.com>
2018-01-20 07:57:09 -0800

    Removed fixtures.js and Documents API

--- imports/startup/server/api.js
+++ imports/startup/server/api.js
@@ -1,5 +1,3 @@
-import '../../api/Documents/server/publications';
-
 import '../../api/OAuth/server/methods';
 
 import '../../api/Users/server/methods';
--- imports/ui/pages/MapDisplay/MapDisplay.js
+++ imports/ui/pages/MapDisplay/MapDisplay.js
@@ -1,11 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import { Table, Alert, Button } from 'react-bootstrap';
-import { Meteor } from 'meteor/meteor';
-import { withTracker } from 'meteor/react-meteor-data';
-import { Bert } from 'meteor/themeteorchef:bert';
-import Loading from '../../components/Loading/Loading';
 
 import './MapDisplay.scss';
 
