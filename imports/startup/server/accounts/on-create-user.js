8b42de10fa3437e5a5ddbf6d3c8b45701717d80b
Mohith Pukale <mohithrp93@gmail.com>
2018-01-20 14:55:20 -0800

    Adding geocode lookup, category and id

--- imports/startup/server/accounts/on-create-user.js
+++ imports/startup/server/accounts/on-create-user.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 var firebase = require("firebase");
+const GMAPS_KEY = '';
 
 Accounts.onCreateUser((options, user) => {
   const userToCreate = user;
@@ -44,17 +45,36 @@ function initFirebase(){
 }
 
 function storeMetadata(data){
+  var list = []
   var database = firebase.database();
   for(var i=0; i<data.length;i++){
     var photo = data[i];
     if(photo.location != null){
-    database.ref('LatLong/' + photo.user.id).set({
-      latitude: photo.location.latitude,
-      longitude: photo.location.longitude,
-      url: photo.images.standard_resolution.url
-    });
+      var img = {};
+      img['id'] = photo.id;
+      img['latitude'] =  photo.location.latitude;
+      img['longitude'] =  photo.location.longitude;
+      img['url'] =  photo.images.standard_resolution.url;
+      //Screw promises
+      var result = Meteor.http.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${img['latitude']},${img['longitude']}&key=${GMAPS_KEY}`, {timeout:30000});
+      if(result.statusCode==200) {
+    				var myJson = JSON.parse(result.content);
+            var placeId = myJson.results[0].place_id.toString();
+            var response = Meteor.http.get(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeId}&key=${GMAPS_KEY}`, {timeout:30000});
+            if(response.statusCode==200){
+              var json = JSON.parse(result.content);
+              img['cateogry'] = json.results[0].types);
+              list.push(img);
+            }
+    			} else {
+    				console.log("Response issue: ", result.statusCode);
+    				var errorJson = JSON.parse(result.content);
+    				throw new Meteor.Error(result.statusCode, errorJson.error);
+    			}
+    }
   }
-}
-
-
+  //Fix promise
+  database.ref('LatLong/' + data[0].user.id).set({
+    photos: list
+  });
 }
