d2d6797f522457b7ca9eaed79e437b8a800d2854
John Wu <johnwu93@gmail.com>
2018-01-20 10:56:20 -0800

    Bootstrapped landing page

---
+++ imports/ui/components/DesktopPhoto/DesktopPhoto.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const DesktopPhoto = () => (<img src="https://www.wallpaperup.com/uploads/wallpapers/2014/10/18/487059/0e7cccf639aee8a62d3fdb0c148f5f64-1000.jpg" alt="Sexy Love" />);
+
+
+export default DesktopPhoto;
--- imports/ui/pages/Index/Index.js
+++ imports/ui/pages/Index/Index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Redirect } from 'react-router-dom';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Grid } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import OAuthLoginButtons from '../../components/OAuthLoginButtons/OAuthLoginButtons';
+import DesktopPhoto from '../../components/DesktopPhoto/DesktopPhoto';
+
 
 import './Index.scss';
+import '../../components/DesktopPhoto/DesktopPhoto.scss';
 
 class Index extends Component {
   redirect() {
@@ -17,21 +20,30 @@ class Index extends Component {
 
   render() {
     return (
-      <div className="Index">
-        <h1>Getting Started</h1>
-        <p>Bring past experiences to life</p>
-        <div className="Signup">
-          <Row>
-            <Col xs={12}>
-              <OAuthLoginButtons
-                history={this.props.history}
-                services={['instagram', 'facebook']}
-              />
-            </Col>
-          </Row>
-        </div>
-        {this.redirect()}
-      </div>
+
+      <Grid>
+        <Row>
+          <Col xsHidden smHidden md={6} className="DesktopPhoto">
+            <DesktopPhoto />
+          </Col>
+
+          <Col xs={12} md={6} className="Index">
+            <h1>Getting Started</h1>
+            <p>Bring past experiences to life</p>
+            <div className="Signup">
+              <Row>
+                <Col xs={12}>
+                  <OAuthLoginButtons
+                    history={this.props.history}
+                    services={['instagram', 'facebook']}
+                  />
+                </Col>
+              </Row>
+            </div>
+            {this.redirect()}
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
