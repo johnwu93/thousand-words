0d794f6c64bd43c74313c90f5adc031b732c9c8c
Victor Li <victorli5674@yahoo.com>
2018-01-20 15:25:25 -0800

    background url for landing page

--- imports/ui/pages/Index/Index.js
+++ imports/ui/pages/Index/Index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Redirect } from 'react-router-dom';
-import { Row, Col, Grid } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import OAuthLoginButtons from '../../components/OAuthLoginButtons/OAuthLoginButtons';
 
@@ -19,9 +19,9 @@ class Index extends Component {
   render() {
     return (
 
-      <Row style={{ backgroundColor: '#7F7F7F' }}>
+      <Row className="row_landing">
         <Col xs={6} xsOffset={3} sm={6} smOffset={3} className="Index" style={{ marginTop: '15vh', marginBottom: '15vh' }}>
-          <h1 style={{ color: '#555555' }}>Getting Started</h1>
+          <h1 style={{ color: '#555' }}>Getting Started</h1>
           <p>Bring past experiences to life</p>
           <div className="Signup">
             <Row>
