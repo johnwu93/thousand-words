e9a0db7dbbdb4d070027c491a86f18eeb0f64d50
Victor Li <victorli5674@yahoo.com>
2018-01-20 14:06:00 -0800

    merge changes

--- imports/ui/pages/Index/Index.js
+++ imports/ui/pages/Index/Index.js
@@ -4,8 +4,6 @@ import { Redirect } from 'react-router-dom';
 import { Row, Col, Grid } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import OAuthLoginButtons from '../../components/OAuthLoginButtons/OAuthLoginButtons';
-import DesktopPhoto from '../../components/DesktopPhoto/DesktopPhoto';
-
 
 import './Index.scss';
 import '../../components/DesktopPhoto/DesktopPhoto.scss';
@@ -21,29 +19,23 @@ class Index extends Component {
   render() {
     return (
 
-      <Grid>
-        <Row>
-          <Col xsHidden smHidden md={6} className="DesktopPhoto">
-            <DesktopPhoto />
-          </Col>
-
-          <Col xs={12} md={6} className="Index">
-            <h1>Getting Started</h1>
-            <p>Bring past experiences to life</p>
-            <div className="Signup">
-              <Row>
-                <Col xs={12}>
-                  <OAuthLoginButtons
-                    history={this.props.history}
-                    services={['instagram', 'facebook']}
-                  />
-                </Col>
-              </Row>
-            </div>
-            {this.redirect()}
-          </Col>
-        </Row>
-      </Grid>
+      <Row style={{ backgroundColor: '#7F7F7F' }}>
+        <Col xs={6} xsOffset={3} sm={6} smOffset={3} className="Index" style={{ marginTop: '15vh', marginBottom: '15vh' }}>
+          <h1 style={{ color: '#555555' }}>Getting Started</h1>
+          <p>Bring past experiences to life</p>
+          <div className="Signup">
+            <Row>
+              <Col xs={12}>
+                <OAuthLoginButtons
+                  history={this.props.history}
+                  services={['instagram', 'facebook']}
+                />
+              </Col>
+            </Row>
+          </div>
+          {this.redirect()}
+        </Col>
+      </Row>
     );
   }
 }
