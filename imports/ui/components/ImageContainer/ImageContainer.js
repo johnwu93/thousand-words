cacb41fe0a725ea577548c9a623eb596a5d9cd6f
Victor Li <victorli5674@yahoo.com>
2018-01-21 01:27:48 -0800

    started image text for images

--- imports/ui/components/ImageContainer/ImageContainer.js
+++ imports/ui/components/ImageContainer/ImageContainer.js
@@ -3,11 +3,14 @@ import { Image } from 'react-bootstrap';
 import './ImageContainer.scss';
 
 const ImageContainer = ({ className, url, type }) => {
+  const mouseOver = (event) => {
+    console.dir(event.target);
+  }
 
   return (
     <div className="ImageContainer">
       <Image className={className} src={url} />
-      <p>{type}</p>
+      <div onMouseOver={mouseOver}>{type}</div>
     </div>
   )
 }
